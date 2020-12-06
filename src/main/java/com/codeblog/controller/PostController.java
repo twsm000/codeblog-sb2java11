06c7e6a0d3490f6c412a510bfef0a68254e50789
Thomas M <twsm000@gmail.com>
2020-12-06 18:36:37 -0300

    Add POST rosouce /newpost and html/form page
     - Conflict caused by @GetMapping removed, methods added to
     @RequestMapping

--- src/main/java/com/codeblog/controller/PostController.java
+++ src/main/java/com/codeblog/controller/PostController.java
@@ -2,12 +2,16 @@ package com.codeblog.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.codeblog.model.Post;
 import com.codeblog.service.DefaultService;
@@ -17,19 +21,32 @@ public class PostController {
     @Autowired
     private DefaultService<Post> service;
 
-    @RequestMapping(value = "/posts")
-    @GetMapping
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
     public ModelAndView getPosts() {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = service.findAll();
         return mv.addObject("posts", posts);
     }
 
-    @RequestMapping(value = "/posts/{id}")
-    @GetMapping
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
     public ModelAndView getPostDetails(@PathVariable(name = "id") Long id) {
         ModelAndView mv = new ModelAndView("postDetails");
         Post post = service.findById(id);        
         return mv.addObject("post", post);
+    }
+    
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String getPostForm() {
+        return "postForm";
+    }
+    
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            return "redirect:/newpost";
+        }
+
+        service.save(post);
+        return "redirect:/posts"; 
     }
 }
