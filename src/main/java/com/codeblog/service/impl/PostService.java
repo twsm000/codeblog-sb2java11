e14086621a2d6add36feedac949dd582f1b597ec
Thomas M <twsm000@gmail.com>
2020-12-06 20:46:46 -0300

    Sort posts in reverse order

--- src/main/java/com/codeblog/service/impl/PostService.java
+++ src/main/java/com/codeblog/service/impl/PostService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.codeblog.model.Post;
@@ -18,7 +19,7 @@ public class PostService implements DefaultService<Post>{
     
     @Override
     public List<Post> findAll() {
-        return repository.findAll();
+        return repository.findAll(Sort.by(Sort.Direction.DESC, "id"));
     }
 
     @Override
